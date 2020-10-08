dee6571d3f7c9b4192164bdd2dbb87556f0740eb
cwalter50 <walt50@gmail.com>
2020-10-08 10:02:31 -0400

    Added a comment

--- QuestionApp/ViewController.swift
+++ QuestionApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+        // Hi coding class!!!!
         view.backgroundColor = UIColor.green
     }
 
